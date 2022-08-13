3eb723cb153817abfc3546efc0b3d3c9f6b36a2e
obby-xiang <liuxiang.obby@qq.com>
2022-08-13 15:56:29 +0800

    初始化

---
+++ app/src/main/java/com/obby/android/blurview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.obby.android.blurview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
